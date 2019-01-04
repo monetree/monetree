0cad001465b67c65f22fe5be38378aae80f60358
soubhagya <soubhagya@recruitmaze.com>
2019-01-04 15:43:21 +0530

    adding changes

--- monetimes/urls.py
+++ monetimes/urls.py
@@ -1,11 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import path
 from landing.views import landing
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('', landing),
 ]
 
