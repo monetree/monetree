3cce90434d74a49983f561eb3af30ccecd4a9024
soubhagya <soubhagya@recruitmaze.com>
2019-01-06 20:10:06 +0530

    adding changes

--- monetimes/settings/__init__.py
+++ monetimes/settings/__init__.py
@@ -1,3 +1,7 @@
-from .production import *
+# from .production import *
 
-# from .local import *
+from .local import *
+
+# from .nginx import *
+
+# from .awsS3 import *
--- monetimes/settings/awsS3.py
+++ monetimes/settings/awsS3.py
@@ -118,10 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-#for local system
+
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static_files"),
 ]
-MEDIA_URL='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = /static/css/bootstrap.min.css(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+
+from monetimes.aws.conf import *
